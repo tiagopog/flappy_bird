7215c13517c4539d93943a64900b3ed6c7e0ca63
Tiago Guedes <tiagopog@gmail.com>
2019-07-10 00:35:46 -0300

    Add ground and manage pipes

--- main.rb
+++ main.rb
@@ -3,12 +3,12 @@ require 'byebug'
 
 
 set title: 'Flappy Bird',
-  width: 640,
-  height: 480,
+  width: 480,
+  height: 640,
   resizable: false,
   background: 'blue'
 
-class Bird < Square
+class Bird < Rectangle
   attr_accessor :flying
 
   def initialize(flying: false, **args)
@@ -31,34 +31,53 @@ class Pipe < Rectangle
   attr_accessor :position
 
   def initialize(position: nil, **args)
-    super(args.merge(width: 100))
+    args.merge!(color: 'green', width: 90)
+    super(args)
     @position = position
   end
 end
 
+class Ground < Rectangle
+  def initialize(**args)
+    super(args)
+  end
+end
+
 class PipeManager
-  attr_reader :pipes
+  attr_reader :pipes, :window
 
   def initialize(window:)
+    @window = window
     @pipes = [
       [
-        Pipe.new(position: :top, height: 200, x: 400),
-        Pipe.new(position: :bottom, height: 50, x: 400, y: window.get(:height) - 50)
+        Pipe.new(position: :top, height: 200, x: window.get(:width) + 200),
+        Pipe.new(position: :bottom, height: 50, x: window.get(:width) + 200, y: window.get(:height) - (80 + 50)),
+        Pipe.new(position: :top, height: 100, x: window.get(:width) + 2 * 200 + 100),
+        Pipe.new(position: :bottom, height: 150, x: window.get(:width) + (2 * 200) + 100, y: window.get(:height) - (80 + 150))
       ]
     ]
   end
 
-  def move!
+  def move_pipes!
     pipes.each do |tuple|
       tuple.each do |pipe|
-        pipe.x -= 1
+        pipe.x -= 2
+        pipe.x = window.get(:width) if pipe.x + pipe.width <= 0
       end
     end
   end
 end
 
+ground = Ground.new(
+  color: 'brown',
+  width: 480,
+  height: 80,
+  y: 640 - 80
+)
+
 bird = Bird.new(
-  size: 50,
+  width: 58,
+  height: 42,
   color: 'yellow',
   x: 30,
   y: get(:height) / 2 - 25,
@@ -68,7 +87,7 @@ pipe_manager = PipeManager.new(window: get(:window))
 
 update do
   bird.y += 2
-  pipe_manager.move!
+  pipe_manager.move_pipes!
 
   on :key_down do |event|
     if !bird.flying? && event.key == 'space'
